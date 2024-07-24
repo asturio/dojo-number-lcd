c268a701a418548525d3d655b903ec88271bdb64
Claudio Clemens <Claudio.Clemens@lv1871.de>
2024-07-24 18:49:22 +0200

    Implementation of Part 1

--- src/main/java/de/lv1871/dojo/converter/NumberToLcd.java
+++ src/main/java/de/lv1871/dojo/converter/NumberToLcd.java
@@ -1,5 +1,97 @@
 package de.lv1871.dojo.converter;
 
 public class NumberToLcd {
+    public static final String EOL = System.lineSeparator();
 
+    public NumberToLcd() {}
+
+    public String[] convertToLcd(int number) {
+        String[] lcd = new String[3];
+        lcd[0] = "";
+        lcd[1] = "";
+        lcd[2] = "";
+        if (number < 10) {
+            lcd = convertDigit(number);
+        } else {
+            String numberAsString = String.valueOf(number);
+            for (char c : numberAsString.toCharArray()) {
+                int i = Integer.parseInt(String.valueOf(c));
+                String[] strings = convertDigit(i);
+                lcd[0] += strings[0];
+                lcd[1] += strings[1];
+                lcd[2] += strings[2];
+            }
+        }
+        return lcd;
+    }
+
+    // Nur einzelne Digits
+    private static String[] convertDigit(int number) {
+        String[] lcd = new String[3];
+        switch (number) {
+            case 1:
+                lcd[0] = "   ";
+                lcd[1] = "  |";
+                lcd[2] = "  |";
+                break;
+            case 2:
+                lcd[0] = " _ ";
+                lcd[1] = " _|";
+                lcd[2] = "|_ ";
+                break;
+            case 3:
+                lcd[0] = " _ ";
+                lcd[1] = " _|";
+                lcd[2] = " _|";
+                break;
+            case 4:
+                lcd[0] = "   ";
+                lcd[1] = "|_|";
+                lcd[2] = "  |";
+                break;
+            case 5:
+                lcd[0] = " _ ";
+                lcd[1] = "|_ ";
+                lcd[2] = " _|";
+                break;
+            case 6:
+                lcd[0] = " _ ";
+                lcd[1] = "|_ ";
+                lcd[2] = "|_|";
+                break;
+            case 7:
+                lcd[0] = " _ ";
+                lcd[1] = "  |";
+                lcd[2] = "  |";
+                break;
+            case 8:
+                lcd[0] = " _ ";
+                lcd[1] = "|_|";
+                lcd[2] = "|_|";
+                break;
+            case 9:
+                lcd[0] = " _ ";
+                lcd[1] = "|_|";
+                lcd[2] = " _|";
+                break;
+            case 0:
+                lcd[0] = " _ ";
+                lcd[1] = "| |";
+                lcd[2] = "|_|";
+                break;
+            default:
+                lcd[0] = "";
+                lcd[1] = "";
+                lcd[2] = "";
+        }
+        return lcd;
+    }
+
+    String toString(String[] zeilen) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : zeilen) {
+            builder.append(s).append(EOL);
+        }
+        return builder.toString();
+    }
 }
--- src/test/java/de/lv1871/dojo/converter/NumberToLcdTest.java
+++ src/test/java/de/lv1871/dojo/converter/NumberToLcdTest.java
@@ -1,7 +1,75 @@
 package de.lv1871.dojo.converter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class NumberToLcdTest {
 
+    private final NumberToLcd cut = new NumberToLcd();
+    private static final String EOL = System.lineSeparator();
 
+    @Test
+    void testDigit0() {
+        // @formatter:off
+        String expected = " _ " + EOL
+                        + "| |" + EOL
+                        + "|_|" + EOL;
+        // @formatter:on
+        assertLcdEquals(cut.convertToLcd(0), expected);
+    }
+
+    @Test
+    void testDigit1() {
+        // @formatter:off
+        String expected = "   " + EOL
+                        + "  |" + EOL
+                        + "  |" + EOL;
+        // @formatter:on
+        assertLcdEquals(cut.convertToLcd(1), expected);
+    }
+
+    @Test
+    void testDigit2() {
+        // @formatter:off
+        String expected = " _ " + EOL
+                        + " _|" + EOL
+                        + "|_ " + EOL;
+        // @formatter:on
+        assertLcdEquals(cut.convertToLcd(2), expected);
+    }
+
+    @Test
+    void testDigitMinus9() {
+        // @formatter:off
+        String expected = EOL +  EOL +  EOL;
+        // @formatter:on
+        assertLcdEquals(cut.convertToLcd(-9), expected);
+    }
+
+    @Test
+    void testNumber12() {
+        // @formatter:off
+        String expected = "    _ " + EOL
+                        + "  | _|" + EOL
+                        + "  ||_ " + EOL;
+        // @formatter:on
+        assertLcdEquals(cut.convertToLcd(12), expected);
+    }
+
+    @Test
+    void testNumber1234567890() {
+        // @formatter:off
+        String expected = "    _  _     _  _  _  _  _  _ "+ EOL
+                        + "  | _| _||_||_ |_   ||_||_|| |"+ EOL
+                        + "  ||_  _|  | _||_|  ||_| _||_|"+ EOL;
+        // @formatter:on
+        assertLcdEquals(cut.convertToLcd(1234567890), expected);
+    }
+
+    private void assertLcdEquals(String[] zeilen, String expected) {
+        assertThat(zeilen).isNotNull();
+        assertThat(cut.toString(zeilen)).isEqualTo(expected);
+    }
 
 }
